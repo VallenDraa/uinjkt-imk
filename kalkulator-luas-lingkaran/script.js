7c0125603c8671e8ffc75e4baf8d152fd414a5b8
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-03-27 08:29:34 +0700

    add history to calculator

--- kalkulator-luas-lingkaran/script.js
+++ kalkulator-luas-lingkaran/script.js
@@ -5,13 +5,18 @@ const angkaDalamRumus = document.getElementById('angka-dalam-rumus');
 const hasilPerhitunganRumus = document.getElementById(
 	'hasil-perhitungan-rumus',
 );
+const historiPerhitungan = JSON.parse(localStorage.getItem('histori')) || [];
+
+inputJariJari.addEventListener('input', () => {
+	const jariJari = inputJariJari.valueAsNumber;
+	tombolHitungLuas.disabled = isNaN(jariJari) || jariJari <= 0;
+});
 
 tombolHitungLuas.addEventListener('click', () => {
 	const jariJari = inputJariJari.valueAsNumber;
 
-	// Validasi input
 	if (isNaN(jariJari) || jariJari <= 0) {
-		alert('Jari-jari harus berupa angka positif');
+		alert('Angka harus lebih dari 0!');
 		return;
 	}
 
@@ -24,9 +29,18 @@ tombolHitungLuas.addEventListener('click', () => {
 	angkaDalamRumus.innerHTML = `L = 3,142 x ${jariJari}<sup>2</sup>`;
 	hasilPerhitunganRumus.innerHTML = `L = ${hasilLuas} cm<sup>2</sup>`;
 
+	// Tambahkan perhitungan ke histori
+	historiPerhitungan.push({
+		jariJari: jariJari,
+		hasilLuas: hasilLuas,
+	});
+	localStorage.setItem('histori', JSON.stringify(historiPerhitungan));
+
+	// Tampilkan histori perhitungan
+	tampilkanHistori();
+
 	// Hapus nilai pada input
 	inputJariJari.value = '';
-	tombolHitungLuas.disabled = true;
 
 	// Scroll ke hasil untuk mengalihkan perhatian user
 	window.scroll({
@@ -34,3 +48,16 @@ tombolHitungLuas.addEventListener('click', () => {
 		behavior: 'smooth',
 	});
 });
+
+function tampilkanHistori() {
+	const historiList = document.getElementById('histori-list');
+	historiList.innerHTML = ''; // Bersihkan histori sebelum menambahkan lagi
+
+	historiPerhitungan.forEach((item, index) => {
+		const historiItem = document.createElement('li');
+		historiItem.innerHTML = `1. Jari-jari = ${item.jariJari}, Luas = ${item.hasilLuas} cm<sup>2</sup>`;
+		historiList.appendChild(historiItem);
+	});
+}
+
+tampilkanHistori();
